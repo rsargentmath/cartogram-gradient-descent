e2eb718205dbb9055e96c0064aa61741603d47e0
rsargentmath <sargent.robert31@gmail.com>
2024-06-11 00:05:00 -0400

    Initial commit

---
+++ cartogram.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+
+def matrix_times_array_of_vectors(matrix, vectors):
+    return (matrix @ vectors[..., np.newaxis])[..., 0]
+
+
+def subdivide_tri(a, b, c, n):
+    verts = [a]
+    tris = []
+    edge_left = np.linspace(a, b, n + 1)
+    edge_right = np.linspace(a, c, n + 1)
+    ix = 0
+    for i in range(n):
+        edge_horiz = np.linspace(edge_left[i + 1], edge_right[i + 1], i + 2)
+        verts += list(edge_horiz)
+        tris.append([ix, ix + i + 1, ix + i + 2])
+        for j in range(i):
+            tris.append([ix + j, ix + i + j + 2, ix + j + 1])
+            tris.append([ix + j + 1, ix + i + j + 2, ix + i + j + 3])
+        ix += i + 1
+    return np.array(verts), np.array(tris)
+
+
+def vec_norm(v):
+    return np.sqrt(v @ v)
+
+
+def nzd(v):
+    return v / vec_norm(v)
+
+
+def tri_area_plane(a, b, c):
+    return 1/2 * vec_norm(np.cross(b-a, c-a))
+
+
+def tri_area_sphere(a, b, c):
+    return 2 * np.arctan(a @ np.cross(b, c) / (1 + a@b + b@c + c@a))
+
+    
+def tri_scale_corrections(a, b, c):
+    r0 = nzd(np.cross(b-a, c-a)) @ a    # distance from abc plane to origin
+    abc_avg = (a + b + c) / 3
+    scale_avg = tri_area_plane(a, b, c) / tri_area_sphere(a, b, c)
+    alpha = 1 / np.sqrt(scale_avg * r0)
+    beta = vec_norm(abc_avg)**(3/2) * alpha
+    return alpha, beta
+
+
+def bary_coords_to_abc_plane(a, b, c, barys):
+    return matrix_times_array_of_vectors(np.column_stack([a, b, c]), barys)
+    
+        
+def subdivide_tri_sphere(a, b, c, n):
+    bary_coords, tris = subdivide_tri(
+        np.array([1, 0, 0]),
+        np.array([0, 1, 0]),
+        np.array([0, 0, 1]),
+        n,
+    )
+    alpha, beta = tri_scale_corrections(a, b, c)
+    a1 = alpha
+    a2 = (3 + 6*alpha - beta - 8*alpha*beta) / (2*beta - 3)
+    a3 = 3 * (-2 - alpha + beta + 2*alpha*beta) / (2*beta - 3)
+    f = lambda x: a1*x + a2*x*x + a3*x*x*x
+    f_vec = lambda v: f(v) / sum(f(v))
+    bary_coords_new = np.apply_along_axis(f_vec, 1, bary_coords)
+    verts = bary_coords_to_abc_plane(a, b, c, bary_coords_new)
+    verts_nzd = np.apply_along_axis(nzd, 1, verts)
+    return verts_nzd, tris
